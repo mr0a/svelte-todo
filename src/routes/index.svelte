2af6c0bb201f95e05b71bfa9b69a67e43c3c3068
Aravindhan <aravindhan2413km@gmail.com>
2021-03-26 02:14:32 +0530

    Todo Good UI

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,14 @@
+<svelte:head>
+	<title>Todo App</title>
+	<meta name="title" content="Todo App" />
+	<meta name="description" content="A todo app with localstorage data saving." />
+</svelte:head>
+
 <script>
-	// import Counter from '$lib/Counter.svelte';
 	import TailwindCss from '$lib/TailwindCSS.svelte';
 
 	let tasks = [];
+	let task='';
 	if (typeof window !== 'undefined'){
 		let tasksFromStorage = localStorage.getItem('tasks');
 		tasks = tasksFromStorage ? JSON.parse(tasksFromStorage): []
@@ -10,16 +16,24 @@
 	
 	$: {
 		if (typeof window !== 'undefined') {
-			document.title = 'ToDo App'
 			window.localStorage.setItem('tasks', JSON.stringify(tasks))
 		}
 	}
 
-	let task='';
 
 	function handleNewTask() {
 		tasks = [...tasks, {title:task, done: false, edit: false}]
 		task = '';
+	}
+
+	function handleClear() {
+		if(typeof window !== undefined){
+			tasks = [];
+		}
+	}
+
+	function removeTask(title) {
+		tasks = tasks.filter((task) => task.title != title )
 	}
 
 </script>
@@ -29,23 +43,29 @@
 	<h1>ToDo!</h1>
 
 	<div class="mb-3 pt-0 w-2/4 m-auto">
-		<input type="text" on:change={handleNewTask} bind:value={task} placeholder="Enter new task" class="px-3 py-3 placeholder-gray-400 text-gray-700 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full"/>
+		<label>
+			<input type="text" on:change={handleNewTask} bind:value={task} placeholder="Enter new task" class="px-3 py-3 rounded text-md shadow outline-none focus:outline-none focus:shadow-outline w-2/3 bg-gray-100 text-gray-700 leading-tight"/>
+		</label>
+		<button on:click={handleClear} class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">Clear</button>
 	</div>
 
 	<div id="tasks">
-		<ul>
-			{#each tasks as task}
-			<li>
-				<input bind:checked={task.done} type="checkbox" >
+		<ul class="px-0 my-5 w-auto">
+			{#each tasks as task,index (task.title)}
+			<li style="position: relative;" class="border border-black list-none {task.done?'bg-blue-400':''} hover:bg-blue-100 rounded-md px-5 py-0">
+				<label style="position: absolute; top: -999px; left: -999px;" for="task-{index}">Check task complete</label>
+				<input id="task-{index}" bind:checked={task.done} type="checkbox" >
 				<p class="font-serif text-lg {task.done ? 'line-through': ''}">{task.title}</p>
+				<p class="text-red-500 hover:text-red-900" style="position: absolute; right: 1px;" on:click={removeTask(task.title)}>x</p>
 			</li>
 			{/each}
 		</ul>
 
 	</div>
-	<!-- <Counter />
 
-	<p>Visit <a href="/mypage">svelte.dev</a> to learn how to build Svelte apps.</p> -->
+	<div>
+		<a href="/mypage">Next Page</a>
+	</div>
 </main>
 
 <style>
@@ -68,28 +88,6 @@
 		line-height: 1.1;
 		margin: 4rem auto;
 		max-width: 14rem;
-	}
-
-	p{
-		display: inline-block;
-	}
-
-	#tasks {
-		max-width: 50%;
-		display: flex;
-		justify-content: center;
-		margin: 0 auto;
-	}
-
-	#tasks ul li{
-		display: flex;
-		align-items: center;
-		border-bottom: 2px solid black;
-		margin-bottom: 10px;
-	}
-
-	#tasks ul li>*{
-		padding: 10px;
 	}
 
 
